bee36dfb1daeb9b5fa1d96fdbfaab389da4bfa12
zhijin.yang <zhijin.yang@macrowing.com>
2021-11-05 16:31:42 +0800

    Signed-off-by: zhijin.yang <zhijin.yang@macrowing.com>

--- WebApplicationTest/Controllers/HomeController.cs
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace WebApplicationTest.Controllers
 
         public IActionResult Index()
         {
+            for (int i = 0; i < 20; i++)
+            {
+
+            }
+
             return View();
         }
 
