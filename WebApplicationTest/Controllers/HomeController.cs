08cb4668248e8839222e9cda77fb075f7b383a15
zhijin.yang <zhijin.yang@macrowing.com>
2021-11-05 16:44:27 +0800

    x

--- WebApplicationTest/Controllers/HomeController.cs
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -21,11 +21,11 @@ namespace WebApplicationTest.Controllers
         public IActionResult Index()
         {
 
-  for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 20; i++)
             {
-
+                Console.WriteLine(i * 3);
             }
-  for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
 
             }
